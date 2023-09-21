93478d5cb031532fe3aa63baa7bd6f946364651d
Karson Tran <karsont@uci.edu>
2023-09-20 17:08:06 -0700

    update random picker

--- source/sudoku.cpp
+++ source/sudoku.cpp
@@ -494,18 +494,14 @@ std::set<int> SudokuBoard::newBoardGenerator::eraseNumOfSquares(int n) {
     return remainingGridNumbers;
 }
 
-int SudokuBoard::newBoardGenerator::pickRanValidVal(std::set<int> &values) {
-    if (!values.size()) return -1;
+int SudokuBoard::newBoardGenerator::pickRanValidVal(std::vector<int> &availValues) {
+    if (!availValues.size()) return -1;
 
     std::random_device generator;
     std::mt19937 rng(generator());
-    std::uniform_int_distribution<std::mt19937::result_type> distribution(0, values.size() - 1);
+    std::uniform_int_distribution<std::mt19937::result_type> distribution(0, availValues.size() - 1);
 
-    auto tempIndex = values.begin();
-
-    std::advance(tempIndex, distribution(generator));
-
-    return *tempIndex;
+    return availValues[distribution(generator)];
 }
 
 void SudokuBoard::newBoardGenerator::insertValueIntoGridSpace(int gridSpace, int value) {
