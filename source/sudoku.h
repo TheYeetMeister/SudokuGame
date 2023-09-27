c989232776435e20f7a63336492cb0981d97e2ab
Karson Tran <karsont@uci.edu>
2023-09-27 10:39:30 -0700

    fix spacing

--- source/sudoku.h
+++ source/sudoku.h
@@ -113,44 +113,44 @@ private:
 
 
 class newBoardGenerator {
-    public:
-        newBoardGenerator(int** newGameBoard, int size, int gridSize);
+public:
+    newBoardGenerator(int** newGameBoard, int size, int gridSize);
 
-        ~newBoardGenerator();
+    ~newBoardGenerator();
 
-        void createCompletedBoard();
+    void createCompletedBoard();
 
-        std::set<int> eraseNumOfSquares(int n);
+    std::set<int> eraseNumOfSquares(int n);
 
-        void getAvailableNumberSet(int gridSpace,
-                                    std::vector<int> &target) const;
+    void getAvailableNumberSet(int gridSpace,
+                                std::vector<int> &target) const;
 
-        bool isUniqueSolution(std::vector<int> &emptyGrids);
-    private:
-        //picks a random number, INCLUDING the number argument given
-        int pickRanVal(int n) const;
+    bool isUniqueSolution(std::vector<int> &emptyGrids);
+private:
+    //picks a random number, INCLUDING the number argument given
+    int pickRanVal(int n) const;
 
-        void insertValueIntoGridSpace(int gridSpace, int value);
+    void insertValueIntoGridSpace(int gridSpace, int value);
 
-        void removeValueFromGridSpace(int gridSpace, int value);
+    void removeValueFromGridSpace(int gridSpace, int value);
 
-        int calRowNumber(int gridSpace) const;
+    int calRowNumber(int gridSpace) const;
 
-        int calColNumber(int gridSpace) const;
+    int calColNumber(int gridSpace) const;
 
-        int calMacroGridCoor(int gridSpace) const;
+    int calMacroGridCoor(int gridSpace) const;
 
-        int calGridNumber(int row, int col) const;
+    int calGridNumber(int row, int col) const;
 
-        bool** rowValues;
-        bool** colValues;
-        bool** grids;
-        std::set<int> remainingGridNumbers;
+    bool** rowValues;
+    bool** colValues;
+    bool** grids;
+    std::set<int> remainingGridNumbers;
 
-        int** newGameBoard;
-        int size;
-        int gridSize;
-        int numberOfAvailableGrids;
-    };
+    int** newGameBoard;
+    int size;
+    int gridSize;
+    int numberOfAvailableGrids;
+};
 
 #endif
