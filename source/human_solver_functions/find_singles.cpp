8d0a42c3c083f29b7f5e5a30ab5c578805c9d45e
Karson Tran <karsont@uci.edu>
2023-09-27 21:31:52 -0700

    initialize 2d bool arrays

--- source/human_solver_functions/find_singles.cpp
+++ source/human_solver_functions/find_singles.cpp
@@ -1,3 +1,15 @@
 #include "find_singles.h"
 
-singlesFinder::singlesFinder(int** gameBoard, int size): gameBoard(gameBoard), size(size) { }
+singlesFinder::singlesFinder(int** gameBoard, int size): gameBoard(gameBoard), size(size) { 
+    availRowValues = new bool*[size]{};
+    availColValues = new bool*[size]{};
+    availMacroGridValues = new bool*[size]{};
+
+    for(int i = 0; i < size; ++i) {
+        availRowValues[i] = new bool[size]{};
+        availColValues[i] = new bool[size]{};
+        availMacroGridValues[i] = new bool[size]{};
+    }
+}
+
+
