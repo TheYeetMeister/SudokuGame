a7298459dc743924fa3084958ec5523d9e1b74dd
Karson Tran <karsont@uci.edu>
2023-10-01 21:18:32 -0700

    move class to new file

--- source/sudoku_generator.h
+++ source/sudoku_generator.h
@@ -1,5 +1,44 @@
 #ifndef SUDOKU_GENERATOR_HPP
 #define SUDOKU_GENERATOR_HPP
 
+class newBoardGenerator {
+public:
+    newBoardGenerator(int** newGameBoard, int size, int gridSize);
 
+    ~newBoardGenerator();
+
+    void createCompletedBoard();
+
+    std::set<int> eraseNumOfSquares(int n);
+
+    void getAvailableNumberSet(int gridSpace,
+                                std::vector<int> &target) const;
+
+    bool isUniqueSolution(std::vector<int> &emptyGrids);
+private:
+    //picks a random number, INCLUDING the number argument given
+    int pickRanVal(int n) const;
+
+    void insertValueIntoGridSpace(int gridSpace, int value);
+
+    void removeValueFromGridSpace(int gridSpace, int value);
+
+    int calRowNumber(int gridSpace) const;
+
+    int calColNumber(int gridSpace) const;
+
+    int calMacroGridCoor(int gridSpace) const;
+
+    int calGridNumber(int row, int col) const;
+
+    bool** rowValues;
+    bool** colValues;
+    bool** grids;
+    std::set<int> remainingGridNumbers;
+
+    int** newGameBoard;
+    int size;
+    int gridSize;
+    int numberOfAvailableGrids;
+};
 #endif
