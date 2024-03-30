9a6ef8d8658ff78db8cfff2509e3437271e5d997
Yeet Meister <ncphias3@gmail.com>
2024-03-29 18:01:30 -0700

    change star img based on difficulty

--- UIFrontEnd/Sudoku/victoryscreen.cpp
+++ UIFrontEnd/Sudoku/victoryscreen.cpp
@@ -29,5 +29,20 @@ void VictoryScreen::displayVictoryScreen() {
 }
 
 void VictoryScreen::displayStars() {
+    std::vector<QLabel*> stars {ui->star1, ui->star2, ui->star3, ui->star4, ui->star5};
+    QString pathToImg;
 
+    switch(difficulty) {
+        case 1:
+            pathToImg = "../../imgs/win_imgs/veryeasy_star.png";
+            break;
+        default:
+            throw std::out_of_range("given difficulty value is out of range");
+    }
+
+    QPixmap starImg(pathToImg);
+    for (unsigned i = 0; i < difficulty; ++i) {
+        stars[i]->setPixmap(starImg);
+        stars[i]->setScaledContents(true);
+    }
 }
